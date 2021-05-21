f5231f2929c4d39cdbd263556ff6b57eb4ff4b46
webliker <18670217003@163.com>
2021-05-21 16:15:32 +0800

    浮点数的精度

---
+++ .history/作用域链/1_20210517103952.js
@@ -0,0 +1,11 @@
+var x = 10
+function fn() {
+  console.log(x)
+}
+function show(f) {
+  var x = 20
+  (function() {
+    f() //10，而不是20
+  })()
+}
+show(fn)
---
+++ .history/作用域链/1_20210517104216.js
@@ -0,0 +1,11 @@
+var x = 10
+function fn() {
+  console.log(x)
+}
+function show(f) {
+  var x = 20
+  f() //10，而不是20
+}
+show(fn)
+
+// 在fn函数中要去自由变量x, 要到创建fn函数的作用域中去取，无论fn在哪里调用
